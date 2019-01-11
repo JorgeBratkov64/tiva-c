5cca33b82bfa4ae48dbeb01ecf66139df4bc770c
Jorge Cervantes <jorge.cervantes.gtz@gmail.com>
2019-01-11 01:08:19 -0600

    ADC_Init defnitions for analog isolation

--- ADC12/adc12.h
+++ ADC12/adc12.h
@@ -49,12 +49,27 @@ typedef uint8_t ADC_CLEAR_DEN__PORTB;
 typedef uint8_t ADC_CLEAR_DEN__PORTD;
 typedef uint8_t ADC_CLEAR_DEN__PORTE;
 
+/* DISABLE ANALOG ISOLATION AMSEL BITS FOR PORT B*/
+#define ADC_AMSEL_AIN10	(1 << 4) 	/* AIN10 -> PB4 */
+#define ADC_AMSEL_AIN11	(1 << 5) 	/* AIN11 -> PB5 */
+
+/* DISABLE ANALOG ISOLATION AFSEL BITS FOR PORT D*/
+#define ADC_AMSEL_AIN7	(1 << 0)	/* AIN7 -> PD0 */
+#define ADC_AMSEL_AIN6	(1 << 1)	/* AIN6 -> PD1 */
+#define ADC_AMSEL_AIN5	(1 << 2)	/* AIN5 -> PD2 */
+#define ADC_AMSEL_AIN4	(1 << 3)	/* AIN4 -> PD3 */
+
+/* DISABLE ANALOG ISOLATION AFSEL BITS FOR PORT E*/
+#define ADC_AMSEL_AIN3	(1 << 0)	/* AIN3 -> PE0 */
+#define ADC_AMSEL_AIN2	(1 << 1)	/* AIN2 -> PE1 */
+#define ADC_AMSEL_AIN1	(1 << 2)	/* AIN1 -> PE2 */
+#define ADC_AMSEL_AIN0	(1 << 3)	/* AIN0 -> PE3 */
+#define ADC_AMSEL_AIN9	(1 << 4)	/* AIN9 -> PE4 */
+#define ADC_AMSEL_AIN8	(1 << 5)	/* AIN8 -> PE5 */
+
 typedef uint8_t ADC_AMSEL__PORTB;
 typedef uint8_t ADC_AMSEL__PORTD;
 typedef uint8_t ADC_AMSEL__PORTE;
-
-
-
 
 typedef enum{
 	ENABLE_ADC_MODULE0 = 1,
