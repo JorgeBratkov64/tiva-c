6d922af92f6f3387a52bad35b4d9cc7402c239ef
Jorge Cervantes <jorge.cervantes.gtz@gmail.com>
2019-01-21 20:51:51 -0600

    Deleted project ADC12

--- ADC12/Objects/adc12_cfg.d
+++
@@ -1,3 +0,0 @@
-.\objects\adc12_cfg.o: adc12_Cfg.c
-.\objects\adc12_cfg.o: adc12.h
-.\objects\adc12_cfg.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- ADC12/Objects/main.d
+++
@@ -1,4 +0,0 @@
-.\objects\main.o: main.c
-.\objects\main.o: adc12.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\main.o: adc12_Cfg.h
